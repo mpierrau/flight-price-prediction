a9b3d0ca4cdf3d92774b9261801fd90a85375f25
Magnus Pierrau <magnus.pierrau@algodx.com>
2024-07-23 18:09:30 +0200

    feat: decapitalize cols, move feature eng to training dir

--- preprocessing/preprocess_data.py
+++ preprocessing/preprocess_data.py
@@ -5,21 +5,14 @@ Written by Magnus Pierrau for MLOps Zoomcamp Final Project Cohort 2024
 """
 
 import logging
-import argparse
 import datetime
 from pathlib import Path
 
+import click
 import pandas as pd
 from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
 from sklearn.model_selection import train_test_split
-
-from preprocessing.feature_engineering import (
-    create_trip_ids,
-    create_weekday_feature,
-    create_total_duration_minutes,
-    create_rounded_arrival_and_departure_times,
-)
 
 logger: logging.Logger = logging.getLogger()
 logger.setLevel(level=logging.INFO)
@@ -45,60 +38,10 @@ def read_csv_file(path: Path) -> pd.DataFrame:
 
 
 @task
-def feature_selection(
-    df: pd.DataFrame,
-    categorical_features: list[str],
-    numerical_features: list[str],
-    target_column: str,
-) -> pd.DataFrame:
-    """
-    A function for extracting only specified features from the given dataframe.
-
-    Args:
-        df (pd.DataFrame): Dataframe with all features
-        categorical_features (list[str]): Which categorical features to include
-        numerical_features (list[str]): Which numerical features to include
-        target_column (str): Name of target column
-    Returns:
-        pd.DataFrame: Dataframe containing only relevant features
-    """
-    df[categorical_features] = df[categorical_features].astype("category")
-    df[numerical_features] = df[numerical_features].astype(int)
-    df = df.filter(
-        items=categorical_features + numerical_features + [target_column],
-        axis="columns",
-    )
-    return df
-
-
-@task
-def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Add a number of new features to the df
-
-    Args:
-        df (pd.DataFrame): Dataframe with flight data
-
-    Returns:
-        pd.DataFrame: Dataframe with added features
-    """
-    df["Weekday"] = create_weekday_feature(df=df)
-    df["TripID"] = create_trip_ids(df=df)
-    df["Departure_hour_rounded"], df["Arrival_hour_rounded"] = (
-        create_rounded_arrival_and_departure_times(df=df)
-    )
-    df["Total_duration_minutes"] = create_total_duration_minutes(df=df)
-
-    return df
-
-
-@task
 def create_table_artifacts(
     df: pd.DataFrame,
-    num_feats: list[str],
-    cat_feats: list[str],
-    target_column: str,
     dataset_name: str,
+    target_column: str = "price",
 ) -> None:
     """
     Creates a number of table artifacts in prefect,
@@ -108,8 +51,19 @@ def create_table_artifacts(
         df (pd.DataFrame): Dataframe to create tables from
         num_feats (list[str]): Names of numerical features
         cat_feats (list[str]): Names of categorical
-        target_column (str): Target column, typically "Price"
+        target_column (str): Target column, typically "price"
     """
+    num_feats = [
+        colname
+        for colname, coltype in df.dtypes.items()
+        if coltype == "int64" and colname != target_column
+    ]
+    cat_feats = [
+        colname
+        for colname, coltype in df.dtypes.items()
+        if coltype == "category" and colname != target_column
+    ]
+
     _ = create_markdown_artifact(
         markdown=df[num_feats].describe().to_markdown(),
         description=f"[{dataset_name}]: Summary table of saved features.",
@@ -168,12 +122,23 @@ def create_test_train_split(
     return train_data, validation_data
 
 
+@click.command
+@click.argument(
+    "path",
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--train-size",
+    type=float,
+    default=0.7,
+    help="Fraction of data to use as training data. \
+        Expected to be between 0 and 1. Test size will be 1-(train-size).",
+)
+@click.option("--random-seed", type=int, default=13371337, help="Random seed for test/train split.")
+@click.option("--savedir", type=Path, default="data", help="Directory where to save data to.")
 @flow(log_prints=True)
 def preprocess_data(
     path: Path,
-    target_column: str,
-    categorical_features: list[str],
-    numerical_features: list[str],
     train_size: float,
     random_seed: int,
     savedir: Path,
@@ -195,28 +160,32 @@ def preprocess_data(
     """
     assert 0 < train_size < 1, f"Got train_size={train_size}. Must be strictly between 0 and 1."
 
-    now = datetime.datetime.now().strftime(format="%Y%m%d_%H%M%S")
-    df = read_csv_file(path)
-    df = feature_engineering(df)
-    df = feature_selection(df, categorical_features, numerical_features, target_column)
+    target_column = "price"
 
-    create_table_artifacts(
-        df, numerical_features, categorical_features, target_column, "final_features"
-    )
-    save_dataset(df, savedir / f"final_features_{now}.parquet")
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    df_raw = read_csv_file(path)
+    df_raw.columns = [x.lower() for x in df_raw.columns]
 
+    # The train/val data is created on the dataframe before creating
+    # the additional features. This is because we want to include
+    # the data preprocessing function in our sklearn pipeline
+    # in the training process.
     data_split = create_test_train_split(
-        df=df,
+        df=df_raw,
         train_size_frac=train_size,
         random_seed=random_seed,
     )
     validation_data, train_data = data_split
 
     create_table_artifacts(
-        validation_data, numerical_features, categorical_features, target_column, "validation_data"
+        validation_data,
+        "validation_data",
+        target_column,
     )
     create_table_artifacts(
-        train_data, numerical_features, categorical_features, target_column, "train_data"
+        train_data,
+        "train_data",
+        target_column,
     )
 
     save_dataset(train_data, savedir / f"train_data_{now}.parquet")
@@ -224,59 +193,4 @@ def preprocess_data(
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=Path, help="Path to data csv file to load.")
-    parser.add_argument(
-        "-cat-feats",
-        "--categorical_features",
-        type=str,
-        nargs="*",
-        default=[
-            "Airline",
-            "Weekday",
-            "TripID",
-            "Departure_hour_rounded",
-            "Arrival_hour_rounded",
-        ],
-        help="Which categorical features to use.",
-    )
-    parser.add_argument(
-        "-num-feats",
-        "--numerical_features",
-        type=str,
-        nargs="*",
-        default=["Total_Stops", "Total_duration_minutes"],
-        help="Which numerical features to use.",
-    )
-    parser.add_argument(
-        "--target", type=str, default="Price", help="Target column for predictions."
-    )
-    parser.add_argument(
-        "--train-size",
-        type=float,
-        default=0.7,
-        help="Fraction of data to use as training data. \
-            Expected to be between 0 and 1. Test size will be 1-(train-size).",
-    )
-    parser.add_argument(
-        "--seed", type=int, default=13371337, help="Random seed for test/train split."
-    )
-    parser.add_argument(
-        "--savedir", type=Path, default="data", help="Directory where to save data to."
-    )
-    args = parser.parse_args()
-
-    kwargs = {
-        "path": args.path,
-        "target_column": args.target,
-        "categorical_features": args.categorical_features,
-        "numerical_features": args.numerical_features,
-        "train_size": args.train_size,
-        "random_seed": args.seed,
-        "savedir": args.savedir,
-    }
-    preprocess_data(**kwargs)
-    # preprocess_data.serve(
-    #     tags=["data_preprocessing", "dev:magnus"],
-    #     parameters=kwargs,
-    # ) # type: ignore
+    preprocess_data()
--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -6,7 +6,7 @@ import pandas as pd
 import deepdiff
 from prefect.testing.utilities import prefect_test_harness
 
-from preprocessing.preprocess_data import feature_selection, feature_engineering
+from training.feature_engineering import feature_selection, feature_engineering
 
 
 def test_preprocess_data() -> None:
@@ -14,39 +14,34 @@ def test_preprocess_data() -> None:
     Test feature engineering and selection functions
     """
     test_data = {
-        "Date": [6, 17],
-        "Month": [8, 7],
-        "Year": [1991, 2024],
-        "Destination": ["Stockholm", "New York"],
-        "Source": ["Birmingham", "St petersburg"],
-        "Dep_hours": [16, 0],
-        "Dep_min": [33, 0],
-        "Arrival_hours": [18, 3],
-        "Arrival_min": [10, 30],
-        "Duration_hours": [2, 12],
-        "Duration_min": [3, 30],
-        "Price": [440, 10000],
+        "airline": ["IndiGo", "IndiGo"],
+        "total_stops": [2, 1],
+        "date": [6, 17],
+        "month": [8, 7],
+        "year": [1991, 2024],
+        "destination": ["Stockholm", "New York"],
+        "source": ["Birmingham", "St petersburg"],
+        "dep_hours": [16, 0],
+        "dep_min": [33, 0],
+        "arrival_hours": [18, 3],
+        "arrival_min": [10, 30],
+        "duration_hours": [2, 12],
+        "duration_min": [3, 30],
+        "price": [440, 10000],
     }
     expected_result_data = {
-        "Weekday": ["Tue", "Wed"],
-        "TripID": ["Stockholm_Birmingham", "NewYork_Stpetersburg"],
-        "Departure_hour_rounded": [17, 0],
-        "Arrival_hour_rounded": [18, 3],
-        "Total_duration_minutes": [123, 750],
-        "Price": [440, 10000],
+        "weekday": ["Tue", "Wed"],
+        "tripid": ["Stockholm_Birmingham", "NewYork_Stpetersburg"],
+        "departure_hour_rounded": [17, 0],
+        "arrival_hour_rounded": [18, 3],
+        "total_duration_minutes": [123, 750],
+        "price": [440, 10000],
     }
     test_df = pd.DataFrame(data=test_data)
 
-    res_df = feature_engineering.fn(df=test_df)
-    res_df = feature_selection.fn(
+    res_df = feature_engineering(df=test_df)
+    res_df = feature_selection(
         df=res_df,
-        categorical_features=["Weekday", "TripID"],
-        numerical_features=[
-            "Departure_hour_rounded",
-            "Arrival_hour_rounded",
-            "Total_duration_minutes",
-        ],
-        target_column="Price",
     )
 
     with prefect_test_harness():
