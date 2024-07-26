00a7ee6cbe370545dd3c6af0bea8d2fca9164e64
Magnus Pierrau <magnus.pierrau@algodx.com>
2024-07-26 10:13:10 +0200

    chore: add blank string as default mlflow uri

--- app/config.py
+++ app/config.py
@@ -15,12 +15,12 @@ class AppSettings(BaseSettings):
     svc_api_port: int = Field(default=8080, validation_alias="SVC_API_PORT")
     """The port to listen to for the service API endpoints."""
 
-    mlflow_model_uri: str | None = Field(
-        default=None,
+    mlflow_model_uri: str = Field(
         validation_alias="MLFLOW_MODEL_URI",
+        default="",
     )
     """The URI to the model to use"""
 
     model_config = SettingsConfigDict(
-        case_sensitive=True, env_file="dotenv", env_file_encoding="utf-8"
+        case_sensitive=True, env_file=".env", env_file_encoding="utf-8"
     )
