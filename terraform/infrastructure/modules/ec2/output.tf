1c88055bad187ca838a165410395f23ad71e8f9c
Magnus Pierrau <magnus.pierrau@algodx.com>
2024-08-08 18:15:43 +0200

    chore: remove unused infra

--- terraform/infrastructure/modules/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.prediction_service.id
-}
-
-output "ip" {
-  description = "Public (Elastic) IP address of the EC2 instance"
-  value = "${aws_eip_association.eip_assoc.public_ip}"
-}
