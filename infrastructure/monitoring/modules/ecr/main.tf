79a5cf80b794607b7b9f70c31d1e5410f7efca9e
Magnus Pierrau <magnus.pierrau@algodx.com>
2024-08-16 13:51:33 +0200

    feat: do scan monitoring ecr image

--- infrastructure/monitoring/modules/ecr/main.tf
+++ infrastructure/monitoring/modules/ecr/main.tf
@@ -18,7 +18,10 @@ resource "aws_ecr_repository" "monitor_repo" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 }
 
