1ccf52d0e2146948348cb7ab0612cebdbaf69d7c
Magnus Pierrau <magnus.pierrau@algodx.com>
2024-08-19 17:46:04 +0200

    fix: skip integration tests until everything is up and running

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -11,7 +11,7 @@ if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     LOCAL_TAG=`date +x"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="flight-price-prediction:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_TAG}"
-    docker build -t ${LOCAL_IMAGE_NAME} ..
+    docker build -t ${LOCAL_IMAGE_NAME} ../infrastructure/sagemaker/app
 else
     echo "no need to build image ${LOCAL_IMAGE_NAME}"
 fi
